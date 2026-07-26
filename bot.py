39adde7efd7520777ed7077082d99343135c543b
roghenedoro1 <Roghenedoro1@gmail.com>
2026-07-26 20:25:45 +0100

    Add print statement for Telegram version

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import os
+import telegram
+print(f"TELEGRAM VERSION: {telegram.__version__}")import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
